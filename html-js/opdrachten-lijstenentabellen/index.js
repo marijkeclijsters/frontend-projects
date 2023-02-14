03241d14736fb5db69cdd8fb78526deaf1a5c6a5
marijkeclijstersPXL <marijke.clijsters@telenet.be>
2023-02-14 22:27:18 +0100

    Toevoeging recept1

--- html-js/opdrachten-lijstenentabellen/index.js
+++ html-js/opdrachten-lijstenentabellen/index.js
@@ -11,15 +11,15 @@ const h1Element = document.createElement('h1');
 h1Element.innerText = "Lievelingsdieren";
 body.appendChild(h1Element);
 
-const olElement = document.createElement('ol');
-body.appendChild(olElement);
+const ulElement = document.createElement('ul');
+body.appendChild(ulElement);
 
 
 const lievelingsdieren =  ["kat", "hond", "papegaai"]
 for (let i = 0; i < 3; i++ ){
     const liElement = document.createElement('li');
     liElement.innerText = lievelingsdieren[i];
-    olElement.appendChild(liElement);
+    ulElement.appendChild(liElement);
 };
 
 //opdracht 2
@@ -27,7 +27,22 @@ const h1ElementRecept = document.createElement('h1');
 h1ElementRecept.innerText = "Recepten";
 body.appendChild(h1ElementRecept);
 
-const h2ElementNaamRecept1 = document.createElement('h2');
-h2ElementNaamRecept1.innerText = "Stoofvlees met frieten";
-body.appendChild(h2ElementNaamRecept1);
+const h2ElementNaamRecept  = document.createElement('h2');
+h2ElementNaamRecept.innerText = "Stoofvlees met frieten";
+body.appendChild(h2ElementNaamRecept);
+
+const imgElement1 = document.createElement('img');
+body.appendChild(imgElement1);
+imgElement1.src = "./stoofvlees-dat-uit-elkaar-valt.jpg";
+body.appendChild(imgElement1);
+
+const h3ElementIngredientRecept = document.createElement('h3');
+h3ElementIngredientRecept.innerText = "Ingrediënten";
+body.appendChild(h3ElementIngredientRecept);
+
+
+
+const h3ElementBereidingRecept = document.createElement('h3');
+h3ElementBereidingRecept.innerText = "Bereiding";
+body.appendChild(h3ElementBereidingRecept);
 
