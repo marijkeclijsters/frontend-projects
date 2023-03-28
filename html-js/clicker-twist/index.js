be82ad5078f6a7e97f2edba3fb652d8dfc1e673e
marijkeclijstersPXL <marijke.clijsters@telenet.be>
2023-03-28 22:01:51 +0200

    adding upgrade button

--- html-js/clicker-twist/index.js
+++ html-js/clicker-twist/index.js
@@ -1,18 +1,28 @@
 const countEl = document.querySelector("#count");
-const countEl2 = document.querySelector("#count2");
+const countElUpgrade = document.querySelector("#upgrade");
 let number = 0;
+let upgradeCounter = 0;
 const button1 = document.querySelector('#button1');
 
 const button2 = document.querySelector('#button2');
 button2.disabled= true;
 
+const button3 = document.querySelector("#button3");
+
+if (number < 10) {
+  button3.disabled = true;
+} else  {
+  button3.disabled = false;
+}
+
+
 function plusOne() {
   number++;
   countEl.textContent = number;
-
   if (number === 10) {
     document.querySelector("#button1").disabled = true;
     button2.disabled = false;
+    button3.disabled = false;
   }
 }
 
@@ -21,4 +31,29 @@ function plusTwo() {
   countEl.textContent = number;
   button2.disabled = true;
   button1.disabled = false;
+  countEl.textContent = number;
 }
+
+
+
+function upgrade() {
+  if (number >= 10) {
+    number = number - 10;
+    upgradeCounter++;
+    countElUpgrade.textContent = upgradeCounter;
+  }
+
+  if (number < 10) {
+    button3.disabled = true;
+     countEl.textContent = number;
+  }
+}
+
+
+// setInterval(() =>{
+//   plusOne();
+//   plusTwo();
+//   upgrade();
+      
+
+//  }, 3000);
